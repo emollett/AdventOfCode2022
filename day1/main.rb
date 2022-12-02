573fcb369a47978637297b9ac943d300510d0d7b
Eleanor Mollett <eleanor.mollett@gmail.com>
2022-12-02 13:39:29 +0000

    Day 1 part 1

--- day1/main.rb
+++ day1/main.rb
@@ -1,15 +1,24 @@
 module Day1
   class Main
-    def initialize(input)
+    attr_accessor :inventory
 
+    def initialize(input)
+      @inventory = input.split("\n\n")
+    end
+
+    def sums
+      sums = []
+      inventory.each do |elf|
+        calories = elf.split("\n").map(&:to_i)
+        sums.push(calories.sum)
+      end
+      sums
     end
 
     def part1
-
+      sums.max
     end
 
-    def part2
-
-    end
+    def part2; end
   end
 end
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -4,16 +4,32 @@ require_relative '../day1/main'
 
 describe Day1::Main do
   let(:input) { File.read('./day1/input.txt').strip }
+  let(:test_input) { File.read('./spec/fixtures/day1.txt').strip }
 
-  describe 'part 1' do
-    let(:expected) { 'Enter your result here' }
+  context 'when using the test data' do
+    let(:puzzle) { Day1::Main.new(test_input) }
 
-    include_examples 'part1'
+    it 'parses the input into inventory' do
+      expect(puzzle.inventory).to eq(%W[1000\n2000\n3000 4000 5000\n6000 7000\n8000\n9000 10000])
+    end
+
+    it 'sums each elfs inventory' do
+      expect(puzzle.sums).to eq([6000, 4000, 11_000, 24_000, 10_000])
+    end
+
+    it 'returns the highest calories' do
+      expect(puzzle.part1).to eq(24_000)
+    end
   end
 
-  describe 'part 2' do
-    let(:expected) { 'Enter your result here' }
+  context 'when using the real data' do
+    let(:puzzle) { Day1::Main.new(input) }
+    it 'returns the answer to part 1' do
+      expect(puzzle.part1).to eq(68_292)
+    end
 
-    include_examples 'part2'
+    it 'returns the answer to part 2' do
+      expect(puzzle.part2).to eq(nil)
+    end
   end
 end
