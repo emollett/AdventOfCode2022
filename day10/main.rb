d3d99c6bb4377476cfd8b55ba02149eaec4dcf72
Eleanor Mollett <eleanor.mollett@gmail.com>
2022-12-11 21:25:46 +0000

    Day 10 part 2

--- day10/main.rb
+++ day10/main.rb
@@ -1,10 +1,11 @@
 module Day10
   class Main
-    attr_accessor :instructions, :interval_values
+    attr_accessor :instructions, :interval_values, :crt
 
     def initialize(input)
       @instructions = input.split("\n")
       @interval_values = []
+      @crt = []
     end
 
     def loop
@@ -12,8 +13,10 @@ module Day10
       x = 1
       i = 0
       addx = false
-      while tick < 221
+      crt_row = ''
+      while tick < 241
         instruction = instructions[i]
+        crt_row = draw_picture(tick, x, crt_row)
         if [20, 60, 100, 140, 180, 220].include?(tick)
           interval_values.push(tick * x)
         end
@@ -32,13 +35,27 @@ module Day10
       x
     end
 
+    def draw_picture(tick, x, crt_row)
+      pixel = (tick-1) % 40
+      sprite = [x-1, x, x+1]
+      sprite.include?(pixel) ? lit = '#' : lit = '.'
+      crt_row += lit
+      if tick % 40 == 0
+        crt.push(crt_row)
+        crt_row = ''
+      end
+      crt_row
+    end
+
     def part1
       loop
       interval_values.sum
     end
 
     def part2
-      nil
+      loop
+      pp crt
+      crt
     end
   end
 end
