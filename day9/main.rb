47070374ea607c398338cb302dedbeaea0c4df87
Eleanor Mollett <eleanor.mollett@gmail.com>
2022-12-14 11:07:33 +0000

    Day 9 part 1

--- day9/main.rb
+++ day9/main.rb
@@ -1,48 +1,59 @@
 module Day9
   class Main
-    attr_accessor :instructions, :location
+    attr_accessor :instructions, :location, :tail_visited
 
     def initialize(input)
-      @instructions = input.split("\n").map{|i| i.split}
-      @location = {head: [0,0], tail: [0,0]}
+      @instructions = input.split("\n").map(&:split)
+      @location = { head: [0, 0], tail: [0, 0] }
+      @tail_visited = ['0,0']
     end
 
     def move
       instructions.each do |instruction|
         instruction[1].to_i.times do
           # move the head
-          case instruction[0]
-          when 'U'
-            location[:head][1] += 1
-          when 'D'
-            location[:head][1] -= 1
-          when 'R'
-            location[:head][0] += 1
-          when 'L'
-            location[:head][0] -= 1
-          end
+          move_head(instruction)
 
-          # move the tail
-          if (location[:head][0] - location[:tail][0]).abs > 1 && (location[:head][1] - location[:tail][1]) != 0
-            # only needs to move by one, not two
-            location[:tail][0] += (location[:head][0] - location[:tail][0])
-            location[:tail][1] += (location[:head][1] - location[:tail][1])
-          elsif (location[:head][1] - location[:tail][1]).abs > 1 && (location[:head][0] - location[:tail][0]) != 0
-            # only needs to move by one, not two
-              location[:tail][0] += (location[:head][0] - location[:tail][0])
-              location[:tail][1] += (location[:head][1] - location[:tail][1])
-          elsif (location[:head][0] - location[:tail][0]).abs  > 1
-            location[:tail][0] += (location[:head][0] - location[:tail][0])
-          elsif (location[:head][1] - location[:tail][1]) > 1
-            location[:tail][1] += (location[:head][1] - location[:tail][1])
-          end
-
+          move_tail
+          tail_visited.push("#{location[:tail][0]},#{location[:tail][1]}")
         end
       end
     end
 
+    def move_head(instruction)
+      case instruction[0]
+      when 'U'
+        location[:head][1] += 1
+      when 'D'
+        location[:head][1] -= 1
+      when 'R'
+        location[:head][0] += 1
+      when 'L'
+        location[:head][0] -= 1
+      end
+    end
+
+    def move_tail
+      x_distance = location[:head][0] - location[:tail][0]
+      y_distance = location[:head][1] - location[:tail][1]
+      distance = Math.sqrt(x_distance.pow(2) + y_distance.pow(2))
+      if distance == 2
+        if x_distance.nonzero?
+          location[:tail][0] += x_distance.positive? ? 1 : -1
+        end
+        if y_distance.nonzero?
+          location[:tail][1] += y_distance.positive? ? 1 : -1
+        end
+      elsif distance > 2
+        #  diagonal move
+        location[:tail][0] += x_distance.positive? ? 1 : -1
+        location[:tail][1] += y_distance.positive? ? 1 : -1
+      end
+    end
+
     def part1
-      nil
+      move
+      tail_visited.uniq.length
     end
 
     def part2
--- spec/day9_spec.rb
+++ spec/day9_spec.rb
@@ -10,21 +10,29 @@ describe Day9::Main do
     let(:puzzle) { described_class.new(test_input) }
 
     it 'gets the instructions' do
-    expect(puzzle.instructions).to eq([["R", "4"], ["U", "4"], ["L", "3"], ["D", "1"], ["R", "4"], ["D", "1"], ["L", "5"], ["R", "2"]])
+      expect(puzzle.instructions).to eq([%w[R 4], %w[U 4], %w[L 3], %w[D 1], %w[R 4], %w[D 1], %w[L 5], %w[R 2]])
     end
 
     it 'moves the head and tail' do
       puzzle.move
-      expect(puzzle.location).to eq({head: [2,2], tail: [1,2]})
+      expect(puzzle.location).to eq({ head: [2, 2], tail: [1, 2] })
     end
 
+    it 'collects all the places the tail visited' do
+      puzzle.move
+      expect(puzzle.tail_visited.uniq).to eq(%w[00 10 20 30 41 42 43 34 24 33 32 22 12])
+    end
+
+    it 'counts the locations visited' do
+      expect(puzzle.part1).to eq(13)
+    end
   end
 
   context 'when using the real data' do
     let(:puzzle) { described_class.new(input) }
 
     it 'returns the answer to part 1' do
-      expect(puzzle.part1).to eq(nil)
+      expect(puzzle.part1).to eq(5878)
     end
 
     it 'returns the answer to part 2' do
